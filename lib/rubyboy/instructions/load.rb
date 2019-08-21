b360984cd67ffa8f5b7827b67c126eda08d548ad
Steven Inskip <inskips@gmail.com>
2019-08-22 00:16:10 +0200

    Reorder load instructions to better group them

--- lib/rubyboy/instructions/load.rb
+++ lib/rubyboy/instructions/load.rb
@@ -7,7 +7,6 @@ module Rubyboy
       def load_nn_n(register)
         value = @mmu.read_short @pc
         set_register(register, value)
-        # @pc += 1
       end
 
       def load_r1_r2(register1, register2)
@@ -25,14 +24,6 @@ module Rubyboy
         @mmu.write_short hl, value
       end
 
-      def ld_a_n() load_nn_n(:a) end
-      def ld_b_n() load_nn_n(:b) end
-      def ld_c_n() load_nn_n(:c) end
-      def ld_d_n() load_nn_n(:d) end
-      def ld_e_n() load_nn_n(:e) end
-      def ld_h_n() load_nn_n(:h) end
-      def ld_l_n() load_nn_n(:l) end
-
       def ld_a_a() load_r1_r2(:a, :a) end
       def ld_a_b() load_r1_r2(:a, :b) end
       def ld_a_c() load_r1_r2(:a, :c) end
@@ -40,6 +31,7 @@ module Rubyboy
       def ld_a_e() load_r1_r2(:a, :e) end
       def ld_a_h() load_r1_r2(:a, :h) end
       def ld_a_l() load_r1_r2(:a, :l) end
+      def ld_a_n() load_nn_n(:a) end
       def ld_a_hl() load_n_hl(:a) end
 
       def ld_b_a() load_r1_r2(:b, :a) end
@@ -49,6 +41,7 @@ module Rubyboy
       def ld_b_e() load_r1_r2(:b, :e) end
       def ld_b_h() load_r1_r2(:b, :h) end
       def ld_b_l() load_r1_r2(:b, :l) end
+      def ld_b_n() load_nn_n(:b) end
       def ld_b_hl() load_n_hl(:b) end
 
       def ld_c_a() load_r1_r2(:c, :a) end
@@ -58,6 +51,7 @@ module Rubyboy
       def ld_c_e() load_r1_r2(:c, :e) end
       def ld_c_h() load_r1_r2(:c, :h) end
       def ld_c_l() load_r1_r2(:c, :l) end
+      def ld_c_n() load_nn_n(:c) end
       def ld_c_hl() load_n_hl(:c) end
 
       def ld_d_a() load_r1_r2(:d, :a) end
@@ -67,6 +61,7 @@ module Rubyboy
       def ld_d_e() load_r1_r2(:d, :e) end
       def ld_d_h() load_r1_r2(:d, :h) end
       def ld_d_l() load_r1_r2(:d, :l) end
+      def ld_d_n() load_nn_n(:d) end
       def ld_d_hl() load_n_hl(:d) end
 
       def ld_e_a() load_r1_r2(:e, :a) end
@@ -76,6 +71,7 @@ module Rubyboy
       def ld_e_e() load_r1_r2(:e, :e) end
       def ld_e_h() load_r1_r2(:e, :h) end
       def ld_e_l() load_r1_r2(:e, :l) end
+      def ld_e_n() load_nn_n(:e) end
       def ld_e_hl() load_n_hl(:e) end
 
       def ld_h_a() load_r1_r2(:h, :a) end
@@ -85,6 +81,7 @@ module Rubyboy
       def ld_h_e() load_r1_r2(:h, :e) end
       def ld_h_h() load_r1_r2(:h, :h) end
       def ld_h_l() load_r1_r2(:h, :l) end
+      def ld_h_n() load_nn_n(:h) end
       def ld_h_hl() load_n_hl(:h) end
 
       def ld_l_a() load_r1_r2(:l, :a) end
@@ -94,6 +91,7 @@ module Rubyboy
       def ld_l_e() load_r1_r2(:l, :e) end
       def ld_l_h() load_r1_r2(:l, :h) end
       def ld_l_l() load_r1_r2(:l, :l) end
+      def ld_l_n() load_nn_n(:l) end
       def ld_l_hl() load_n_hl(:l) end
 
       def ld_hl_a() load_hl_n(:a) end
